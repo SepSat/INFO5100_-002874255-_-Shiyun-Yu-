1641089d37cd8e8965f62151d6af4310d193ec2c
SepSat <yushiyun1998@gmail.com>
2023-10-21 21:52:20 -0700

    Exercise 1 submission

---
+++ Exercises/exercise1/src/PartTimeStudent.java
@@ -0,0 +1,5 @@
+public class PartTimeStudent extends Student {
+    PartTimeStudent(String name,float[] quizScore) {
+        super(name,quizScore);
+    }
+}
---
+++ Exercises/exercise1/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    String name;
+    float[] quizScores;
+    public Student(String name,float[] quizScore){
+        this.name = name;
+        this.quizScores = quizScore;
+    }
+}
