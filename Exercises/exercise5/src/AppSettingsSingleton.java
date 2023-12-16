a6e12cdefcbdefe37d21a17a3f6bbb68e5eeff93
SepSat <yushiyun1998@gmail.com>
2023-12-15 20:07:35 -0800

    Exercise 5 Submission

---
+++ Exercises/exercise5/src/AppSettingsSingleton.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// Singleton Class
+public class AppSettingsSingleton {
+    private static AppSettingsSingleton instance = new AppSettingsSingleton();
+
+    // Configuration settings can be stored in a HashMap
+    private Map<String, String> settings;
+
+    // Constructor is private to prevent instantiation from outside
+    private AppSettingsSingleton() {
+        settings = new HashMap<>();
+        // Load settings from a file, database, or hardcoded values
+        settings.put("theme", "Light");
+        settings.put("language", "English");
+    }
+
+    public static AppSettingsSingleton getInstance() {
+        return instance;
+    }
+
+    public String getSetting(String key) {
+        return settings.getOrDefault(key, "Not Found");
+    }
+
+    public void setSetting(String key, String value) {
+        settings.put(key, value);
+    }
+}
---
+++ Exercises/exercise5/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+// Main class to demonstrate Singleton
+public class Main {
+    public static void main(String[] args) {
+        // Singleton example:
+        singleton();
+        observer();
+        factory();
+    }
+
+    private static void singleton(){
+        AppSettingsSingleton settings = AppSettingsSingleton.getInstance();
+
+        System.out.println("Current theme: " + settings.getSetting("theme"));
+        System.out.println("Current language: " + settings.getSetting("language"));
+
+        // Change a setting
+        settings.setSetting("theme", "Dark");
+        System.out.println("New theme: " + settings.getSetting("theme"));
+    }
+    private static void observer(){
+        StockMarketTracker marketTracker = new StockMarketTracker();
+
+        StockDisplay display1 = new StockDisplay("Observer 1");
+        StockDisplay display2 = new StockDisplay("Observer 2");
+
+        marketTracker.addStockObserver(display1);
+        marketTracker.addStockObserver(display2);
+
+        // Simulating stock price changes
+        marketTracker.setStockPrice("AAPL", 150.5f);
+        marketTracker.setStockPrice("MSFT", 250.75f);
+    }
+
+    private static void factory(){
+        VehicleFactory factory = new VehicleFactory();
+
+        Vehicle myCar = factory.getVehicle("CAR");
+        myCar.startEngine();
+
+        Vehicle myMotorbike = factory.getVehicle("MOTORBIKE");
+        myMotorbike.startEngine();
+    }
+}
